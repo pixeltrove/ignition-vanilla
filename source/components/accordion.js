239d03558c2712feb28222883f1942a6e578c99b
Luchian <luchiancosma@gmail.com>
2021-03-16 19:36:27 +0200

    Enable accordion navigation using arrow and jump keys
    Navigating accordions with the aid of the `Up`, `Down`, `Home` and `End` keys is a welcome enhancement for keyboard users.

--- source/components/accordion.js
+++ source/components/accordion.js
@@ -9,6 +9,8 @@ const DATA_TARGET = "data-target";
 
 const Accordion = (accordion) => {
   const handles = accordion.querySelectorAll(SELECTOR_HANDLE);
+  const firstHandle = handles[0];
+  const lastHandle = handles[handles.length - 1];
 
   const toggle = (event) => {
     const trigger = event.currentTarget;
@@ -21,7 +23,52 @@ const Accordion = (accordion) => {
     collapsible.classList.toggle(CLASS_SHOWN);
   };
 
-  handles.forEach((handle) => handle.addEventListener("click", toggle));
+  const handleArrowKeys = (event) => {
+    let currentHandle = null;
+    let previousHandle = null;
+
+    switch (event.key) {
+      case "ArrowUp":
+        event.preventDefault();
+
+        currentHandle = document.activeElement;
+        currentHandleIndex = Array.from(handles).indexOf(currentHandle);
+        previousHandle = Array.from(handles)[currentHandleIndex - 1];
+
+        if (currentHandle === firstHandle) {
+          lastHandle.focus();
+        } else {
+          previousHandle.focus();
+        }
+        break;
+      case "ArrowDown":
+        event.preventDefault();
+
+        currentHandle = document.activeElement;
+        currentHandleIndex = Array.from(handles).indexOf(currentHandle);
+        nextHandle = Array.from(handles)[currentHandleIndex + 1];
+
+        if (currentHandle === lastHandle) {
+          firstHandle.focus();
+        } else {
+          nextHandle.focus();
+        }
+        break;
+      case "Home":
+        event.preventDefault();
+        firstHandle.focus();
+        break;
+      case "End":
+        event.preventDefault();
+        lastHandle.focus();
+        break;
+    }
+  };
+
+  accordion.addEventListener("keydown", handleArrowKeys);
+  handles.forEach((handle) => {
+    handle.addEventListener("click", toggle);
+  });
 };
 
 const accordions = document.querySelectorAll(SELECTOR_ACCORDION);
