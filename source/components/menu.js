d45ef93603a1d1d36741653a7e42d56f2601cc79
Luchian <luchiancosma@gmail.com>
2021-04-06 11:36:57 +0300

    Enable navigation using arrow and jump keys
    Navigating menus with the aid of the `Up`, `Down`, `Home` and `End` keys is a welcome enhancement for keyboard users.

--- source/components/menu.js
+++ source/components/menu.js
@@ -2,6 +2,7 @@
 // -----------------------------------------------------------------------------
 
 const SELECTOR_MENU = ".menu";
+const SELECTOR_LINK = ".menu-link";
 const CLASS_ACTIVATED = "is-activated";
 const CLASS_SHOWN = "is-shown";
 const DATA_TARGET = "data-target";
@@ -9,6 +10,9 @@ const DATA_TARGET = "data-target";
 const Menu = (menu) => {
   const menuId = menu.id;
   const trigger = document.querySelector(`[${DATA_TARGET}="${menuId}"]`);
+  const links = menu.querySelectorAll(SELECTOR_LINK);
+  const firstLink = links[0];
+  const lastLink = links[links.length - 1];
 
   const toggle = () => {
     const isExpanded = menu.classList.contains(CLASS_SHOWN);
@@ -21,10 +25,47 @@ const Menu = (menu) => {
       document.addEventListener("click", hideOnOutsideClick);
       trigger.addEventListener("keydown", hideOnTab);
       menu.addEventListener("keydown", hideOnTab);
+      links.forEach((link) => {
+        link.addEventListener("keydown", navigateUsingKeyboard);
+      });
     } else {
       document.removeEventListener("click", hideOnOutsideClick);
       trigger.removeEventListener("keydown", hideOnTab);
       menu.removeEventListener("keydown", hideOnTab);
+    }
+  };
+
+  const navigateUsingKeyboard = (event) => {
+    const currentLink = event.currentTarget;
+    const currentLinkIndex = Array.from(links).indexOf(currentLink);
+    const previousLink = links[currentLinkIndex - 1];
+    const nextLink = links[currentLinkIndex + 1];
+
+    switch (event.key) {
+      case "ArrowUp":
+        event.preventDefault();
+        if (currentLink === firstLink) {
+          lastLink.focus();
+        } else {
+          previousLink.focus();
+        }
+        break;
+      case "ArrowDown":
+        event.preventDefault();
+        if (currentLink === lastLink) {
+          firstLink.focus();
+        } else {
+          nextLink.focus();
+        }
+        break;
+      case "Home":
+        event.preventDefault();
+        firstLink.focus();
+        break;
+      case "End":
+        event.preventDefault();
+        lastLink.focus();
+        break;
     }
   };
 
