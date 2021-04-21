93601aef5033eb7871a036d98367c66d40e89278
Luchian <luchiancosma@gmail.com>
2021-04-21 12:44:08 +0300

    Define specialized functions for event handling
    Decisions on whether actions related to a specific event should happen are best handled separately, via specialized functions, in order to make code easier to understand and manage.

--- source/components/accordion.js
+++ source/components/accordion.js
@@ -11,50 +11,56 @@ function Accordion(accordion) {
   const handles = Array.from(accordion.querySelectorAll(SELECTOR_HANDLE));
 
   function togglePanel(event) {
-    if (event.target.closest(SELECTOR_HANDLE)) {
-      const currentHandle = event.target;
-      const panelId = currentHandle.getAttribute(DATA_TARGET);
-      const panel = document.querySelector(`#${panelId}`);
-      const isShown = panel.classList.contains(CLASS_SHOWN);
+    const currentHandle = event.target;
+    const panelId = currentHandle.getAttribute(DATA_TARGET);
+    const panel = document.querySelector(`#${panelId}`);
+    const isShown = panel.classList.contains(CLASS_SHOWN);
 
-      currentHandle.classList.toggle(CLASS_ACTIVATED);
-      currentHandle.setAttribute("aria-expanded", !isShown);
-      panel.classList.toggle(CLASS_SHOWN);
-    }
+    currentHandle.classList.toggle(CLASS_ACTIVATED);
+    currentHandle.setAttribute("aria-expanded", !isShown);
+    panel.classList.toggle(CLASS_SHOWN);
   }
 
   function moveFocus(event) {
-    if (event.target.closest(SELECTOR_HANDLE)) {
-      const keys = ["ArrowUp", "ArrowDown", "Home", "End"];
-      const currentIndex = handles.indexOf(event.target);
-      const lastIndex = handles.length - 1;
-      let upcomingIndex = 0;
+    const keys = ["ArrowUp", "ArrowDown", "Home", "End"];
+    const currentIndex = handles.indexOf(event.target);
+    const lastIndex = handles.length - 1;
+    let upcomingIndex = 0;
 
-      if (keys.includes(event.key)) {
-        event.preventDefault();
+    if (keys.includes(event.key)) {
+      event.preventDefault();
 
-        switch (event.key) {
-          case "ArrowUp":
-            upcomingIndex = currentIndex === 0 ? lastIndex : currentIndex - 1;
-            break;
-          case "ArrowDown":
-            upcomingIndex = currentIndex === lastIndex ? 0 : currentIndex + 1;
-            break;
-          case "Home":
-            upcomingIndex = 0;
-            break;
-          case "End":
-            upcomingIndex = lastIndex;
-            break;
-        }
-
-        handles[upcomingIndex].focus();
+      switch (event.key) {
+        case "ArrowUp":
+          upcomingIndex = currentIndex === 0 ? lastIndex : currentIndex - 1;
+          break;
+        case "ArrowDown":
+          upcomingIndex = currentIndex === lastIndex ? 0 : currentIndex + 1;
+          break;
+        case "Home":
+          upcomingIndex = 0;
+          break;
+        case "End":
+          upcomingIndex = lastIndex;
+          break;
       }
+
+      handles[upcomingIndex].focus();
     }
   }
 
-  accordion.addEventListener("click", togglePanel);
-  accordion.addEventListener("keydown", moveFocus);
+  function manageClick(event) {
+    if (!event.target.closest(SELECTOR_HANDLE)) return;
+    togglePanel(event);
+  }
+
+  function manageKeydown(event) {
+    if (!event.target.closest(SELECTOR_HANDLE)) return;
+    moveFocus(event);
+  }
+
+  accordion.addEventListener("click", manageClick);
+  accordion.addEventListener("keydown", manageKeydown);
 }
 
 const accordions = Array.from(document.querySelectorAll(SELECTOR_ACCORDION));
