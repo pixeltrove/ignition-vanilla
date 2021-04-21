ca48859b87cdfa9188f0424a6b744e998ce57ba2
Luchian <luchiancosma@gmail.com>
2021-04-21 09:58:21 +0300

    Listen for events at the component root level
    Event listeners should generally be attached to components not their constituents in order to allow listening for dynamically added elements.

--- source/components/accordion.js
+++ source/components/accordion.js
@@ -11,48 +11,50 @@ function Accordion(accordion) {
   const handles = Array.from(accordion.querySelectorAll(SELECTOR_HANDLE));
 
   function togglePanel(event) {
-    const currentHandle = event.currentTarget;
-    const panelId = currentHandle.getAttribute(DATA_TARGET);
-    const panel = document.querySelector(`#${panelId}`);
-    const isShown = panel.classList.contains(CLASS_SHOWN);
+    if (event.target.closest(SELECTOR_HANDLE)) {
+      const currentHandle = event.target;
+      const panelId = currentHandle.getAttribute(DATA_TARGET);
+      const panel = document.querySelector(`#${panelId}`);
+      const isShown = panel.classList.contains(CLASS_SHOWN);
 
-    currentHandle.classList.toggle(CLASS_ACTIVATED);
-    currentHandle.setAttribute("aria-expanded", !isShown);
-    panel.classList.toggle(CLASS_SHOWN);
-  }
-
-  function moveFocus(event) {
-    const keys = ["ArrowUp", "ArrowDown", "Home", "End"];
-    const currentIndex = handles.indexOf(event.currentTarget);
-    const lastIndex = handles.length - 1;
-    let upcomingIndex = 0;
-
-    if (keys.includes(event.key)) {
-      event.preventDefault();
-
-      switch (event.key) {
-        case "ArrowUp":
-          upcomingIndex = currentIndex === 0 ? lastIndex : currentIndex - 1;
-          break;
-        case "ArrowDown":
-          upcomingIndex = currentIndex === lastIndex ? 0 : currentIndex + 1;
-          break;
-        case "Home":
-          upcomingIndex = 0;
-          break;
-        case "End":
-          upcomingIndex = lastIndex;
-          break;
-      }
-
-      handles[upcomingIndex].focus();
+      currentHandle.classList.toggle(CLASS_ACTIVATED);
+      currentHandle.setAttribute("aria-expanded", !isShown);
+      panel.classList.toggle(CLASS_SHOWN);
     }
   }
 
-  handles.forEach((handle) => {
-    handle.addEventListener("click", togglePanel);
-    handle.addEventListener("keydown", moveFocus);
-  });
+  function moveFocus(event) {
+    if (event.target.closest(SELECTOR_HANDLE)) {
+      const keys = ["ArrowUp", "ArrowDown", "Home", "End"];
+      const currentIndex = handles.indexOf(event.target);
+      const lastIndex = handles.length - 1;
+      let upcomingIndex = 0;
+
+      if (keys.includes(event.key)) {
+        event.preventDefault();
+
+        switch (event.key) {
+          case "ArrowUp":
+            upcomingIndex = currentIndex === 0 ? lastIndex : currentIndex - 1;
+            break;
+          case "ArrowDown":
+            upcomingIndex = currentIndex === lastIndex ? 0 : currentIndex + 1;
+            break;
+          case "Home":
+            upcomingIndex = 0;
+            break;
+          case "End":
+            upcomingIndex = lastIndex;
+            break;
+        }
+
+        handles[upcomingIndex].focus();
+      }
+    }
+  }
+
+  accordion.addEventListener("click", togglePanel);
+  accordion.addEventListener("keydown", moveFocus);
 }
 
 const accordions = Array.from(document.querySelectorAll(SELECTOR_ACCORDION));
