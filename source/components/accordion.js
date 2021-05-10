373622eed77b1455c16c8a823130666debcaae1b
Luchian <luchiancosma@gmail.com>
2021-05-10 08:56:12 +0300

    Rename accordion toggles and event handlers

--- source/components/accordion.js
+++ source/components/accordion.js
@@ -2,33 +2,27 @@
 // -----------------------------------------------------------------------------
 
 const SELECTOR_ACCORDION = ".accordion";
-const SELECTOR_HANDLE = ".accordion-handle";
+const SELECTOR_SLAT = ".accordion-slat";
 const CLASS_ACTIVATED = "is-activated";
 const CLASS_SHOWN = "is-shown";
 const DATA_TARGET = "data-target";
 
 function Accordion(accordion) {
-  const handles = Array.from(accordion.querySelectorAll(SELECTOR_HANDLE));
+  const slats = Array.from(accordion.querySelectorAll(SELECTOR_SLAT));
 
-  function togglePanel(handle) {
-    const panelId = handle.getAttribute(DATA_TARGET);
+  function togglePanel(slat) {
+    const panelId = slat.getAttribute(DATA_TARGET);
     const panel = document.querySelector(`#${panelId}`);
     const isShown = panel.classList.contains(CLASS_SHOWN);
 
-    handle.classList.toggle(CLASS_ACTIVATED);
-    handle.setAttribute("aria-expanded", !isShown);
+    slat.classList.toggle(CLASS_ACTIVATED);
+    slat.setAttribute("aria-expanded", !isShown);
     panel.classList.toggle(CLASS_SHOWN);
   }
 
-  function managePanelToggle(event) {
-    if (event.target.closest(SELECTOR_HANDLE)) {
-      togglePanel(event.target.closest(SELECTOR_HANDLE));
-    }
-  }
-
-  function moveFocus(handle, key) {
-    const currentIndex = handles.indexOf(handle);
-    const lastIndex = handles.length - 1;
+  function moveFocus(slat, key) {
+    const currentIndex = slats.indexOf(slat);
+    const lastIndex = slats.length - 1;
     let upcomingIndex;
 
     switch (key) {
@@ -46,18 +40,24 @@ function Accordion(accordion) {
         break;
     }
 
-    handles[upcomingIndex].focus();
+    slats[upcomingIndex].focus();
   }
 
-  function manageFocusMove(event) {
-    if (event.target.closest(SELECTOR_HANDLE) && ["ArrowUp", "ArrowDown", "Home", "End"].includes(event.key)) {
-      event.preventDefault();
-      moveFocus(event.target.closest(SELECTOR_HANDLE), event.key);
+  function handleSlatClick(event) {
+    if (event.target.closest(SELECTOR_SLAT)) {
+      togglePanel(event.target.closest(SELECTOR_SLAT));
     }
   }
 
-  accordion.addEventListener("click", managePanelToggle);
-  accordion.addEventListener("keydown", manageFocusMove);
+  function handleSlatKeydown(event) {
+    if (event.target.closest(SELECTOR_SLAT) && ["ArrowUp", "ArrowDown", "Home", "End"].includes(event.key)) {
+      event.preventDefault();
+      moveFocus(event.target.closest(SELECTOR_SLAT), event.key);
+    }
+  }
+
+  accordion.addEventListener("click", handleSlatClick);
+  accordion.addEventListener("keydown", handleSlatKeydown);
 }
 
 const accordions = Array.from(document.querySelectorAll(SELECTOR_ACCORDION));
